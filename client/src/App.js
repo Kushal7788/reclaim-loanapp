813586c460da26ff5d3a01ddfb26a91a87df5158
Kushal7788 <kushshah777888@gmail.com>
2023-07-14 13:22:00 +0530

    main heading spelling correction

--- client/src/App.js
+++ client/src/App.js
@@ -45,7 +45,7 @@ function App() {
             <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
               <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
                 ScamChecker, <br class="hidden lg:inline-block" />A new way to
-                proof Identity using ZK
+                prove Identity using ZK
               </h1>
               <p class="mb-8 leading-relaxed">
                 ScamChecker is a new way to prove Identity using ZK. Verify your client credentials in a secure way using Reclaim Protocol based on secure ZK Proofs. Click on Verify Credentials to get started.
